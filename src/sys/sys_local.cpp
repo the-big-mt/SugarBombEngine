86c227f2b1ca6322520bbe8f948a8e68f29bed12
BlackPhrase <BlackPhrase@users.noreply.github.com>
2019-01-30 13:12:08 +0000

    [sys] Setup pointers to other modules during the module initialization

--- src/sys/sys_local.cpp
+++ src/sys/sys_local.cpp
@@ -55,6 +55,18 @@ static sysExport_t sysExport;
 idSysLocal			sysLocal;
 idSys* 				sys = &sysLocal;
 
+// TODO
+/*
+#ifndef SBE_SINGLE_BINARY
+idSys *sys = nullptr;
+idCommon *common = nullptr;
+idCmdSystem *cmdSystem = nullptr;
+idCVarSystem *cvarSystem = nullptr;
+idFileSystem *fileSystem = nullptr;
+idCVar *idCVar::staticVars = nullptr;
+#endif
+*/
+
 /*
 ===========
 GetSysAPI
@@ -67,10 +79,25 @@ sysExport_t *GetSysAPI(sysImport_t *import)
 {
 	if(import->version == SYS_API_VERSION)
 	{
-		// set interface pointers used by the system
-		
 		// TODO
+		/*
+		// set interface pointers used by the module
+		sys							= import->sys;
+		common						= import->common;
+		cmdSystem					= import->cmdSystem;
+		cvarSystem					= import->cvarSystem;
+		fileSystem					= import->fileSystem;
+		*/
 	};
+	
+	// TODO
+	/*
+	// set interface pointers used by idLib
+	idLib::sys					= sys;
+	idLib::common				= common;
+	idLib::cvarSystem			= cvarSystem;
+	idLib::fileSystem			= fileSystem;
+	*/
 	
 	// setup export interface
 	sysExport.version = SYS_API_VERSION;
