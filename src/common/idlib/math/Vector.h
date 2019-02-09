e9624fcc2e5629873f7a86e738308623d8a16b7b
BlackPhrase <BlackPhrase@users.noreply.github.com>
2019-02-09 10:30:17 +0000

    [common/idlib] Fix includes in Vector header

--- src/common/idlib/math/Vector.h
+++ src/common/idlib/math/Vector.h
@@ -29,7 +29,7 @@ If you have questions concerning this license or the applicable additional terms
 #ifndef __MATH_VECTOR_H__
 #define __MATH_VECTOR_H__
 
-//#include <cstring>                     // for memcpy
+#include <cstring> // for memcpy
 
 //#include "../idlib/containers/Array.h" // for idTupleSize
 //#include "../idlib/math/Math.h"
