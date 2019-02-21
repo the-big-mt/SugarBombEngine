004b8c230d058c41d6bdae077c6632fd989afdff
BlackPhrase <BlackPhrase@users.noreply.github.com>
2019-02-21 14:13:43 +0000

    [f3] Add F3 actor values definitions (imported from vaultmp code)

--- src/f3/Actor.h
+++ src/f3/Actor.h
@@ -392,6 +392,91 @@ private:
 	std::unique_ptr<CharacterController> mCharacterController;
 	// vaultmp
 public:
+	// F3-specific actor values
+	// TODO: FNV merged big and small guns together
+	enum class Values
+	{
+		Aggression = 0x00,
+		Confidence = 0x01,
+		Energy = 0x02,
+		Responsibility = 0x03,
+		Mood = 0x04,
+		
+		Strength = 0x05,
+		Perception = 0x06,
+		Endurance = 0x07,
+		Charisma = 0x08,
+		Intelligence = 0x09,
+		Agility = 0x0A,
+		Luck = 0x0B,
+		
+		ActionPoints = 0x0C,
+		CarryWeight = 0x0D,
+		CritChance = 0x0E,
+		HealRate = 0x0F,
+		Health = 0x10,
+		MeleeDamage = 0x11,
+		DamageResistance = 0x12,
+		PoisonResistance = 0x13,
+		RadResistance = 0x14,
+		SpeedMultiplier = 0x15,
+		Fatigue = 0x16,
+		Karma = 0x17,
+		XP = 0x18,
+		
+		Head = 0x19,
+		Torso = 0x1A,
+		LeftArm = 0x1B,
+		RightArm = 0x1C,
+		LeftLeg = 0x1D,
+		RightLeg = 0x1E,
+		Brain = 0x1F,
+		
+		Barter = 0x20,
+		BigGuns = 0x21,
+		EnergyWeapons = 0x22,
+		Explosives = 0x23,
+		Lockpick = 0x24,
+		Medicine = 0x25,
+		MeleeWeapons = 0x26,
+		Repair = 0x27,
+		Science = 0x28,
+		SmallGuns = 0x29,
+		Sneak = 0x2A,
+		Speech = 0x2B,
+		Throwing = 0x2C,
+		Unarmed = 0x2D,
+		
+		InventoryWeight = 0x2E,
+		Paralysis = 0x2F,
+		Invisibility = 0x30,
+		Chameleon = 0x31,
+		NightEye = 0x32,
+		DetectLifeRange = 0x33,
+		FireResistance = 0x34,
+		WaterBreathing = 0x35,
+		RadLevel = 0x36,
+		BloodyMess = 0x37,
+		UnarmedDamage = 0x38,
+		Assistance = 0x39,
+
+		EnergyResistance = 0x3C,
+		EMPResistance = 0x3D,
+		
+		Var1Medical = 0x3E,
+		Variable02 = 0x3F,
+		Variable03 = 0x40,
+		Variable04 = 0x41,
+		Variable05 = 0x42,
+		Variable06 = 0x43,
+		Variable07 = 0x44,
+		Variable08 = 0x45,
+		Variable09 = 0x46,
+		Variable10 = 0x47,
+		
+		IgnoreCrippledLimbs = 0x48,
+	};
+
 	static const std::map<unsigned char, std::pair<const float, const float>> default_values;
 private:
 	std::unordered_map<unsigned char, Value<float>> Values;
