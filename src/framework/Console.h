42fa8cdad606fe9d48602d8db0db702ef76062f7
BlackPhrase <BlackPhrase@users.noreply.github.com>
2019-03-31 11:37:59 +0000

    [framework] Extract idConsole interface (2)

--- src/framework/Console.h
+++ src/framework/Console.h
@@ -3,6 +3,7 @@
 
 Doom 3 BFG Edition GPL Source Code
 Copyright (C) 1993-2012 id Software LLC, a ZeniMax Media company.
+Copyright (C) 2018-2019 BlackPhrase
 
 This file is part of the Doom 3 BFG Edition GPL Source Code ("Doom 3 BFG Edition Source Code").
 
@@ -29,67 +30,7 @@ If you have questions concerning this license or the applicable additional terms
 #ifndef __CONSOLE_H__
 #define __CONSOLE_H__
 
-enum justify_t
-{
-	JUSTIFY_LEFT,
-	JUSTIFY_RIGHT,
-	JUSTIFY_CENTER_LEFT,
-	JUSTIFY_CENTER_RIGHT
-};
-
-class idOverlayHandle
-{
-	friend class idConsoleLocal;
-public:
-	idOverlayHandle() : index( -1 ), time( 0 ) {}
-private:
-	int		index;
-	int		time;
-};
-
-/*
-===============================================================================
-
-	The console is strictly for development and advanced users. It should
-	never be used to convey actual game information to the user, which should
-	always be done through a GUI.
-
-	The force options are for the editor console display window, which
-	doesn't respond to pull up / pull down
-
-===============================================================================
-*/
-
-class idConsole
-{
-public:
-	virtual			~idConsole() {}
-	
-	virtual void	Init() = 0;
-	virtual void	Shutdown() = 0;
-	
-	virtual bool	ProcessEvent( const sysEvent_t* event, bool forceAccept ) = 0;
-	
-	// the system code can release the mouse pointer when the console is active
-	virtual bool	Active() = 0;
-	
-	// clear the timers on any recent prints that are displayed in the notify lines
-	virtual void	ClearNotifyLines() = 0;
-	
-	// force console open
-	virtual void	Open() = 0;
-	
-	// some console commands, like timeDemo, will force the console closed before they start
-	virtual void	Close() = 0;
-	
-	virtual void	Draw( bool forceFullScreen ) = 0;
-	virtual void	Print( const char* text ) = 0;
-	
-	virtual void	PrintOverlay( idOverlayHandle& handle, justify_t justify, VERIFY_FORMAT_STRING const char* text, ... ) = 0;
-	
-	virtual idDebugGraph* 	CreateGraph( int numItems ) = 0;
-	virtual void			DestroyGraph( idDebugGraph* graph ) = 0;
-};
+#include "framework/IConsole.hpp"
 
 extern idConsole* 	console;	// statically initialized to an idConsoleLocal
 
