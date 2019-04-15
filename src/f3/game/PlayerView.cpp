e10000b5abbee05c1effdbf86b44b0c3253b70c8
BlackPhrase <BlackPhrase@users.noreply.github.com>
2019-04-15 14:01:23 +0000

    [f3/game] Draw HUD in third person view (taken from Classic-RBDOOM-3-BFG repo)

--- src/f3/game/PlayerView.cpp
+++ src/f3/game/PlayerView.cpp
@@ -3,6 +3,7 @@
 
 Doom 3 BFG Edition GPL Source Code
 Copyright (C) 1993-2012 id Software LLC, a ZeniMax Media company.
+Copyright (C) 2019 BlackPhrase
 
 This file is part of the Doom 3 BFG Edition GPL Source Code ("Doom 3 BFG Edition Source Code").
 
@@ -472,7 +473,7 @@ void idPlayerView::SingleView( const renderView_t* view, idMenuHandler_HUD* hudM
 	}
 	
 	// draw screen blobs
-	if( !pm_thirdPerson.GetBool() && !g_skipViewEffects.GetBool() )
+	if( /*!pm_thirdPerson.GetBool() &&*/ !g_skipViewEffects.GetBool() )
 	{
 		if( !player->spectating )
 		{
