81196fa57055ee694f92ec06f7c5cb3dff737103
BlackPhrase <BlackPhrase@users.noreply.github.com>
2019-04-30 05:22:01 +0000

    Import vaultmp's vaultmpdll code

---
+++ code/f3/vaultmpdll/vaultmp.hpp
@@ -0,0 +1,9 @@
+// Fallout 3 addresses
+
+static const unsigned int LOOKUP_FORM        = 0x00455190; // Credit goes to FOSE
+static const unsigned int LOOKUP_FUNC        = 0x00519AF0;
+static const unsigned int QUEUE_UI_MESSAGE   = 0x0061B850; // Credit goes to FOSE
+static const unsigned int ALERTED_STATE      = 0x006F6C70;
+static const unsigned int SNEAKING_STATE     = 0x006F58B0;
+static const unsigned int SETPOS             = 0x006F2050;
+
