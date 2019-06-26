478945a83941c46fff758aace07da4b5cbbc673c
BlackPhrase <BlackPhrase@users.noreply.github.com>
2019-06-26 07:52:17 +0000

    [common/network] Fix INetworkSystem header includes

--- src/common/network/INetworkSystem.hpp
+++ src/common/network/INetworkSystem.hpp
@@ -40,7 +40,7 @@ If you have questions concerning this license or the applicable additional terms
 ==============================================================
 */
 
-#include "network/NetworkTypes.hpp"
+#include "NetworkTypes.hpp"
 
 /*
 ================================================
