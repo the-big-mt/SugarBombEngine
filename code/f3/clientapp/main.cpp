a6bd035cfa1bfcc186f55a18007236296925811f
BlackPhrase <BlackPhrase@users.noreply.github.com>
2019-07-04 11:32:59 +0000

    [f3/clientapp] Mix idTech4's main with OpenMW's together

--- code/f3/clientapp/main.cpp
+++ code/f3/clientapp/main.cpp
@@ -6,15 +6,6 @@
 
 #include "engine.hpp"
 
-#if defined(_WIN32)
-#ifndef WIN32_LEAN_AND_MEAN
-#define WIN32_LEAN_AND_MEAN
-#endif
-#include <windows.h>
-// makes __argc and __argv available on windows
-#include <cstdlib>
-#endif
-
 #if (defined(__APPLE__) || defined(__linux) || defined(__unix) || defined(__posix))
 #include <unistd.h>
 #endif
@@ -262,22 +253,3 @@ int runApplication(int argc, char *argv[])
 
     return 0;
 }
-
-#ifdef ANDROID
-extern "C" int SDL_main(int argc, char**argv)
-#else
-int main(int argc, char**argv)
-#endif
-{
-    return wrapApplication(&runApplication, argc, argv, "OpenMW");
-}
-
-// Platform specific for Windows when there is no console built into the executable.
-// Windows will call the WinMain function instead of main in this case, the normal
-// main function is then called with the __argc and __argv parameters.
-#if defined(_WIN32) && !defined(_CONSOLE)
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
-{
-    return main(__argc, __argv);
-}
-#endif
