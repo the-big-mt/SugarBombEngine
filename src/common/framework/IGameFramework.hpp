dc59e0a7ca8f1dfdb2f6f1ee21164d8930183aba
BlackPhrase <BlackPhrase@users.noreply.github.com>
2019-07-05 15:11:15 +0000

    [common/framework] Update IGameFramework license header

--- src/common/framework/IGameFramework.hpp
+++ src/common/framework/IGameFramework.hpp
@@ -6,7 +6,7 @@ Copyright (C) 2014-2016 Robert Beckebans
 Copyright (C) 2014-2016 Kot in Action Creative Artel
 Copyright (C) 2019 SugarBombEngine Developers
 
-This file is part of SugarBombEngine.
+This file is part of SugarBombEngine
 
 SugarBombEngine is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -22,10 +22,12 @@ You should have received a copy of the GNU General Public License
 along with SugarBombEngine. If not, see <http://www.gnu.org/licenses/>.
 
 In addition, SugarBombEngine is using id Tech 4 (BFG) pieces and thus
-subject to certain additional terms. You should have received a copy
-of these additional terms immediately following the terms and conditions
-of the GNU General Public License which accompanied the id Tech 4 source code.
-If not, please request a copy in writing from id Software at the address below.
+subject to certain additional terms (all header and source files which 
+contains such pieces has this additional part appended to the license 
+header). You should have received a copy of these additional terms 
+stated in a separate file (LICENSE-idTech4) which accompanied the 
+SugarBombEngine source code. If not, please request a copy in 
+writing from id Software at the address below.
 
 If you have questions concerning this license or the applicable additional
 terms, you may contact in writing id Software LLC, c/o ZeniMax Media Inc.,
