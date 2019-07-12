4644b82b53348281e8ab9087b12c60bf1f7f485f
BlackPhrase <BlackPhrase@users.noreply.github.com>
2019-07-12 12:46:49 +0000

    [SbSound] Update stub sound sample code to use ISys methods to get the language count/name

--- src/SbSound/stub/SbSoundSample.cpp
+++ src/SbSound/stub/SbSoundSample.cpp
@@ -255,15 +255,15 @@ void idSoundSample::LoadResource()
 				
 				if( sampleName.Find( "/vo/" ) >= 0 )
 				{
-					for( int i = 0; i < Sys_NumLangs(); i++ )
+					for( int i = 0; i < mpSys->GetLangsNum(); i++ ) // TODO: GetLangCount?
 					{
-						const char* lang = Sys_Lang( i );
+						const char* lang = mpSys->GetLangName( i ); // TODO: replace with sys_lang cvar?
 						if( idStr::Icmp( lang, ID_LANG_ENGLISH ) == 0 )
 						{
 							continue;
 						}
 						idStrStatic< MAX_OSPATH > locName = GetName();
-						locName.Replace( "/vo/", va( "/vo/%s/", Sys_Lang( i ) ) );
+						locName.Replace( "/vo/", va( "/vo/%s/", mpSys->GetLangName( i ) ) ); // TODO: replace with sys_lang cvar?
 						WriteAllSamples( locName );
 					}
 				}
