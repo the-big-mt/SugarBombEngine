ce090d54dae3892209bc8b7b3fbc039c3399f58f
BlackPhrase <BlackPhrase@users.noreply.github.com>
2019-09-24 14:21:52 +0000

    [sugarbomb/SDK/SbSound] Update SoundTypes header's license header

--- code/sugarbomb/SDK/CoreLibs/SbSound/SoundTypes.hpp
+++ code/sugarbomb/SDK/CoreLibs/SbSound/SoundTypes.hpp
@@ -2,7 +2,7 @@
 *******************************************************************************
 
 Copyright (C) 1993-2012 id Software LLC, a ZeniMax Media company.
-Copyright (C) 2018-2019 BlackPhrase
+Copyright (C) 2018-2019 SugarBombEngine Developers
 
 This file is part of SugarBombEngine
 
