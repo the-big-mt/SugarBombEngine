2087b4b1909d8ae9c4f39c7237f5d56fdd501864
BlackPhrase <BlackPhrase@users.noreply.github.com>
2019-10-14 15:42:59 +0000

    [F3GOATY/GameApp] Implement the app (currently does the same as the SbSample)

---
+++ f3goaty/GameApp/main.cpp
@@ -0,0 +1,23 @@
+/// @file
+
+#include <cstdlib>
+
+#ifdef _WIN32
+#	include <windows.h>
+#endif
+
+#include "AppFrameworks/SbClientApp/SbClientApp.hpp"
+
+int main(int argc, char **argv)
+{
+	SbClientApp App(argc, argv);
+	App.Run();
+	return EXIT_SUCCESS;
+};
+
+#ifdef _WIN32
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	return main(__argc, __argv);
+};
+#endif
