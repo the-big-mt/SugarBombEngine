9cbe4b65d7752261b8562c7a9d74f76267e2a012
BlackPhrase <BlackPhrase@users.noreply.github.com>
2019-10-14 15:02:43 +0000

    [sugarbomb/SDK] Add SbClientApp class header

---
+++ sugarbomb/SDK/Include/AppFrameworks/SbClientApp/SbClientApp.hpp
@@ -0,0 +1,12 @@
+/// @file
+
+#pragma once
+
+#include "AppFrameworks/SbApplication/SbApplication.hpp"
+
+class SbClientApp : public SbApplication
+{
+public:
+	SbClientApp();
+	virtual ~SbClientApp() = default;
+};
