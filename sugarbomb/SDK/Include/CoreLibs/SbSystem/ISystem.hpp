aa8a304fe1cd51c997f908deb2d4c19713511cc1
BlackPhrase <BlackPhrase@users.noreply.github.com>
2019-10-15 13:31:48 +0000

    [sugarbomb/SDK/SbSystem] Update ISystem interface with dynamic libs management functionality

--- sugarbomb/SDK/Include/CoreLibs/SbSystem/ISystem.hpp
+++ sugarbomb/SDK/Include/CoreLibs/SbSystem/ISystem.hpp
@@ -12,6 +12,19 @@ struct ISystem
 	
 	///
 	virtual void Shutdown() = 0;
+	
+	///
+	virtual int LoadLib(const char *asPath) = 0;
+	
+	///
+	virtual void FreeLib(int anHandle) = 0;
+	
+	///
+	virtual void *GetLibSymbol(int anHandle, const char *asSymbol) const = 0;
+	
+	///
+	template<typename T>
+	T GetLibSymbol(int anHandle, const char *asSymbol) const {return reinterpret_cast<T>(GetLibSymbol(anHandle, asSymbol));}
 };
 
 }; // namespace sbe
