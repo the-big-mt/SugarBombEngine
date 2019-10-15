ef21521b18dbcd95d57de11d362d11bd8b48fe85
BlackPhrase <BlackPhrase@users.noreply.github.com>
2019-10-15 12:49:59 +0000

    [SbGameFramework] Load and init the game module

--- sugarbomb/SbGameFramework/SbGameFramework.cpp
+++ sugarbomb/SbGameFramework/SbGameFramework.cpp
@@ -2,15 +2,48 @@
 
 #include "SbGameFramework.hpp"
 
+#include "CoreLibs/SbSystem/ISystem.hpp"
+
+#include "SbGame/IGame.hpp"
+
 namespace sbe::SbGameFramework
 {
 
+SbGameFramework::SbGameFramework(ISystem &aSystem) : mSystem(aSystem){}
+
 void SbGameFramework::Init()
 {
+	LoadGameModule();
+	
+	mpGame->Init();
 };
 
 void SbGameFramework::Shutdown()
 {
+	mpGame->Shutdown();
+	
+	UnloadGameModule();
+};
+
+void SbGameFramework::LoadGameModule()
+{
+	mnGameLib = mSystem.get().LoadLib("SbGame");
+	
+	if(!mnGameLib)
+		return;
+	
+	using fnGetGameAPI = IGame *(*)();
+	fnGetGameAPI pfnGetGameAPI{mSystem.get().GetLibSymbol<fnGetGameAPI>(mnGameLib, "GetGameAPI")};
+	
+	if(!pfnGetGameAPI)
+		return;
+	
+	mpGame = pfnGetGameAPI();
+};
+
+void SbGameFramework::UnloadGameModule()
+{
+	mSystem.get().FreeLib(mnGameLib);
 };
 
 }; // namespace sbe::SbGameFramework
--- sugarbomb/SbGameFramework/SbGameFramework.hpp
+++ sugarbomb/SbGameFramework/SbGameFramework.hpp
@@ -2,16 +2,35 @@
 
 #pragma once
 
+#include <functional>
+
 #include "SbGameFramework/IGameFramework.hpp"
 
-namespace sbe::SbGameFramework
+namespace sbe
+{
+
+struct ISystem;
+struct IGame;
+
+namespace SbGameFramework
 {
 
 class SbGameFramework : public IGameFramework
 {
 public:
+	SbGameFramework(ISystem &aSystem);
+	
 	void Init() override;
 	void Shutdown() override;
+private:
+	void LoadGameModule();
+	void UnloadGameModule();
+	
+	std::reference_wrapper<ISystem> mSystem;
+	
+	IGame *mpGame{nullptr};
+	
+	int mnGameLib{-1};
 };
 
-}; // namespace sbe::SbGameFramework
+};}; // namespace sbe::SbGameFramework
