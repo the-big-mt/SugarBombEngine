489d38685059b8a6448e33d97d88c1abbb41f98e
BlackPhrase <BlackPhrase@users.noreply.github.com>
2019-10-15 13:14:34 +0000

    [SbApplication] Load and init the system module

--- sugarbomb/AppFrameworks/SbApplication/SbApplication.cpp
+++ sugarbomb/AppFrameworks/SbApplication/SbApplication.cpp
@@ -1,12 +1,23 @@
 /// @file
 
 #include <cassert>
+#include <stdexcept>
 
 #include "AppFrameworks/SbApplication/SbApplication.hpp"
+#include "SbLibraryLoader.hpp"
+
+#include "CoreLibs/SbSystem/ISystem.hpp"
 
 SbApplication::SbApplication(int argc, char **argv)
 {
 	Init();
+};
+
+SbApplication::~SbApplication()
+{
+	mpSystem->Shutdown();
+	
+	UnloadSystemModule();
 };
 
 void SbApplication::Run()
@@ -21,7 +32,35 @@ void SbApplication::Run()
 
 void SbApplication::Init()
 {
+	LoadSystemModule();
+	
+	mpSystem->Init();
+	
 	mbInitialized = true;
+};
+
+void SbApplication::LoadSystemModule()
+{
+	mnSystemLib = sbe::SbLibraryLoader::Load("SbSystem");
+	
+	if(!mnSystemLib)
+		throw std::runtime_error("Failed to load the system module!");
+	
+	using fnGetSystemAPI = sbe::ISystem *(*)();
+	fnGetSystemAPI pfnGetSystemAPI{sbe::SbLibraryLoader::GetSymbol<fnGetSystemAPI>(mnSystemLib, "GetSystemAPI")};
+	
+	if(!pfnGetSystemAPI)
+		throw std::runtime_error("");
+	
+	mpSystem = pfnGetSystemAPI();
+	
+	if(!mpSystem)
+		throw std::runtime_error("");
+};
+
+void SbApplication::UnloadSystemModule()
+{
+	sbe::SbLibraryLoader::Unload(mnSystemLib);
 };
 
 void SbApplication::RunFrame()
--- sugarbomb/SDK/Include/AppFrameworks/SbApplication/SbApplication.hpp
+++ sugarbomb/SDK/Include/AppFrameworks/SbApplication/SbApplication.hpp
@@ -2,17 +2,29 @@
 
 #pragma once
 
+namespace sbe
+{
+struct ISystem;
+};
+
 class SbApplication
 {
 public:
 	SbApplication(int argc, char **argv);
-	virtual ~SbApplication() = default;
+	virtual ~SbApplication();
 	
 	void Run();
+protected:
+	sbe::ISystem *mpSystem{nullptr};
 private:
 	void Init();
+	
+	void LoadSystemModule();
+	void UnloadSystemModule();
 	
 	void RunFrame();
 	
 	bool mbInitialized{false};
+	
+	int mnSystemLib{-1};
 };
