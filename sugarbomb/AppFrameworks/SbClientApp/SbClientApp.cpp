fd5aafbc18993d73b04a040790ff5817d4b11314
BlackPhrase <BlackPhrase@users.noreply.github.com>
2019-10-15 13:11:31 +0000

    [SbClientApp] Load and init input and render modules

--- sugarbomb/AppFrameworks/SbClientApp/SbClientApp.cpp
+++ sugarbomb/AppFrameworks/SbClientApp/SbClientApp.cpp
@@ -1,7 +1,81 @@
 /// @file
 
+#include <stdexcept>
+
 #include "AppFrameworks/SbClientApp/SbClientApp.hpp"
+
+#include "CoreLibs/SbSystem/ISystem.hpp"
+
+#include "CoreLibs/SbRenderer/IRenderSystem.hpp"
+
+#include "AppFrameworks/UtilityLibs/SbInput/IInputSystem.hpp"
 
 SbClientApp::SbClientApp(int argc, char **argv) : SbApplication(argc, argv)
 {
+	LoadRenderSystemModule();
+	
+	mpRenderSystem->Init();
+	
+	LoadInputSystemModule();
+	
+	mpInputSystem->Init();
+};
+
+SbClientApp::~SbClientApp()
+{
+	mpInputSystem->Shutdown();
+	
+	UnloadInputSystemModule();
+	
+	mpRenderSystem->Shutdown();
+	
+	UnloadRenderSystemModule();
+};
+
+void SbClientApp::LoadRenderSystemModule()
+{
+	mnRenderSystemLib = mpSystem->LoadLib("SbGLCoreRenderer");
+	
+	if(!mnRenderSystemLib)
+		throw std::runtime_error("Failed to load the renderer module!");
+	
+	using fnGetRenderSystemAPI = sbe::IRenderSystem *(*)();
+	fnGetRenderSystemAPI pfnGetRenderSystemAPI{mpSystem->GetLibSymbol<fnGetRenderSystemAPI>(mnRenderSystemLib, "GetRenderSystemAPI")};
+	
+	if(!pfnGetRenderSystemAPI)
+		throw std::runtime_error("");
+	
+	mpRenderSystem = pfnGetRenderSystemAPI();
+	
+	if(!mpRenderSystem)
+		throw std::runtime_error("");
+};
+
+void SbClientApp::UnloadRenderSystemModule()
+{
+	mpSystem->FreeLib(mnRenderSystemLib);
+};
+
+void SbClientApp::LoadInputSystemModule()
+{
+	mnInputSystemLib = mpSystem->LoadLib("SbInput");
+	
+	if(!mnInputSystemLib)
+		throw std::runtime_error("Failed to load the input module!");
+	
+	using fnGetInputSystemAPI = sbe::IInputSystem *(*)();
+	fnGetInputSystemAPI pfnGetInputSystemAPI{mpSystem->GetLibSymbol<fnGetInputSystemAPI>(mnInputSystemLib, "GetInputSystemAPI")};
+	
+	if(!pfnGetInputSystemAPI)
+		throw std::runtime_error("");
+	
+	mpInputSystem = pfnGetInputSystemAPI();
+	
+	if(!mpInputSystem)
+		throw std::runtime_error("");
+};
+
+void SbClientApp::UnloadInputSystemModule()
+{
+	mpSystem->FreeLib(mnInputSystemLib);
 };
--- sugarbomb/SDK/Include/AppFrameworks/SbClientApp/SbClientApp.hpp
+++ sugarbomb/SDK/Include/AppFrameworks/SbClientApp/SbClientApp.hpp
@@ -4,9 +4,27 @@
 
 #include "AppFrameworks/SbApplication/SbApplication.hpp"
 
+namespace sbe
+{
+struct IRenderSystem;
+struct IInputSystem;
+};
+
 class SbClientApp : public SbApplication
 {
 public:
 	SbClientApp(int argc, char **argv);
-	virtual ~SbClientApp() = default;
+	virtual ~SbClientApp();
+private:
+	void LoadRenderSystemModule();
+	void UnloadRenderSystemModule();
+	
+	void LoadInputSystemModule();
+	void UnloadInputSystemModule();
+	
+	sbe::IRenderSystem *mpRenderSystem{nullptr};
+	sbe::IInputSystem *mpInputSystem{nullptr};
+	
+	int mnRenderSystemLib{-1};
+	int mnInputSystemLib{-1};
 };
