4e3a443f46aa27985ba4c1a3cf9af4765f47ba2f
BlackPhrase <BlackPhrase@users.noreply.github.com>
2019-10-23 12:56:46 +0000

    [SbSound] Init and shutdown the sound hardware

--- sugarbomb/CoreLibs/SbSound/SbSoundSystem.cpp
+++ sugarbomb/CoreLibs/SbSound/SbSoundSystem.cpp
@@ -1,7 +1,9 @@
+void SbSoundSystem::Init()
 /*
 *******************************************************************************
 
-Copyright (C) 2019 SugarBombEngine Developers
+Copyright (C) 1993-2012 id Software LLC, a ZeniMax Media company.
+Copyright (C) 2018-2019 SugarBombEngine Developers
 
 This file is part of SugarBombEngine
 
@@ -18,22 +20,35 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with SugarBombEngine. If not, see <http://www.gnu.org/licenses/>.
 
+In addition, SugarBombEngine is using id Tech 4 (BFG) pieces and thus
+subject to certain additional terms (all header and source files which 
+contains such pieces has this additional part appended to the license 
+header). You should have received a copy of these additional terms 
+stated in a separate file (LICENSE-idTech4) which accompanied the 
+SugarBombEngine source code. If not, please request a copy in 
+writing from id Software at the address below.
+
+If you have questions concerning this license or the applicable additional
+terms, you may contact in writing id Software LLC, c/o ZeniMax Media Inc.,
+Suite 120, Rockville, Maryland 20850 USA.
+
 *******************************************************************************
 */
 
 /// @file
 
 #include "SbSoundSystem.hpp"
+#include "SbSoundHardware.hpp"
 
 namespace sbe::SbSound
 {
 
-void SbSoundSystem::Init()
 {
+	mHardware.Init();
 };
 
 void SbSoundSystem::Shutdown()
 {
+	mHardware.Shutdown();
 };
-
 }; // namespace sbe::SbSound
