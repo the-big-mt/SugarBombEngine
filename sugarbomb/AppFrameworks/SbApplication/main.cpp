a5dc542c55a242f3d37767cf9a952a3bc44041df
BlackPhrase <BlackPhrase@users.noreply.github.com>
2020-03-20 12:34:56 +0000

    [SbApplication] The main function will now be defined here so devs will only work with platform-independent one

---
+++ sugarbomb/AppFrameworks/SbApplication/main.cpp
@@ -0,0 +1,42 @@
+/*
+*******************************************************************************
+
+Copyright (C) 2020 SugarBombEngine Developers
+
+This file is part of SugarBombEngine
+
+SugarBombEngine is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+SugarBombEngine is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with SugarBombEngine. If not, see <http://www.gnu.org/licenses/>.
+
+*******************************************************************************
+*/
+
+/// @file
+
+#include "AppFrameworks/SbApplication/SbApplication.hpp"
+
+#ifdef _WIN32
+#	include <windows.h>
+#endif
+
+int main(int argc, char **argv)
+{
+	return SbApplication::Main(argc, argv);
+};
+
+#ifdef _WIN32
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	return main(__argc, __argv);
+};
+#endif
