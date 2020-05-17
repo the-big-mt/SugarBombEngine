6f59ad06c2280b188b0db0c3a18cf4dd167d21fe
BlackPhrase <BlackPhrase@users.noreply.github.com>
2020-05-17 13:11:40 +0000

    [common/framework] Doxygen comments (3)

--- src/common/framework/ICVarSystem.hpp
+++ src/common/framework/ICVarSystem.hpp
@@ -99,26 +99,26 @@ namespace sbe
 
 typedef enum
 {
-	CVAR_ALL				= -1,		// all flags
-	CVAR_BOOL				= BIT( 0 ),	// variable is a boolean
-	CVAR_INTEGER			= BIT( 1 ),	// variable is an integer
-	CVAR_FLOAT				= BIT( 2 ),	// variable is a float
-	CVAR_SYSTEM				= BIT( 3 ),	// system variable
-	CVAR_RENDERER			= BIT( 4 ),	// renderer variable
-	CVAR_SOUND				= BIT( 5 ),	// sound variable
-	CVAR_GUI				= BIT( 6 ),	// gui variable
-	CVAR_GAME				= BIT( 7 ),	// game variable
-	CVAR_TOOL				= BIT( 8 ),	// tool variable
+	CVAR_ALL				= -1,		///< all flags
+	CVAR_BOOL				= BIT( 0 ),	///< variable is a boolean
+	CVAR_INTEGER			= BIT( 1 ),	///< variable is an integer
+	CVAR_FLOAT				= BIT( 2 ),	///< variable is a float
+	CVAR_SYSTEM				= BIT( 3 ),	///< system variable
+	CVAR_RENDERER			= BIT( 4 ),	///< renderer variable
+	CVAR_SOUND				= BIT( 5 ),	///< sound variable
+	CVAR_GUI				= BIT( 6 ),	///< gui variable
+	CVAR_GAME				= BIT( 7 ),	///< game variable
+	CVAR_TOOL				= BIT( 8 ),	///< tool variable
 	// original doom3 used to have CVAR_USERINFO ("sent to servers, available to menu") here
-	CVAR_SERVERINFO			= BIT( 10 ),	// sent from servers, available to menu
-	CVAR_NETWORKSYNC		= BIT( 11 ),	// cvar is synced from the server to clients
-	CVAR_STATIC				= BIT( 12 ),	// statically declared, not user created
-	CVAR_CHEAT				= BIT( 13 ),	// variable is considered a cheat
-	CVAR_NOCHEAT			= BIT( 14 ),	// variable is not considered a cheat
-	CVAR_INIT				= BIT( 15 ),	// can only be set from the command-line
-	CVAR_ROM				= BIT( 16 ),	// display only, cannot be set by user at all
-	CVAR_ARCHIVE			= BIT( 17 ),	// set to cause it to be saved to a config file
-	CVAR_MODIFIED			= BIT( 18 )	// set when the variable is modified
+	CVAR_SERVERINFO			= BIT( 10 ),	///< sent from servers, available to menu
+	CVAR_NETWORKSYNC		= BIT( 11 ),	///< cvar is synced from the server to clients
+	CVAR_STATIC				= BIT( 12 ),	///< statically declared, not user created
+	CVAR_CHEAT				= BIT( 13 ),	///< variable is considered a cheat
+	CVAR_NOCHEAT			= BIT( 14 ),	///< variable is not considered a cheat
+	CVAR_INIT				= BIT( 15 ),	///< can only be set from the command-line
+	CVAR_ROM				= BIT( 16 ),	///< display only, cannot be set by user at all
+	CVAR_ARCHIVE			= BIT( 17 ),	///< set to cause it to be saved to a config file
+	CVAR_MODIFIED			= BIT( 18 )	///< set when the variable is modified
 } cvarFlags_t;
 
 /*
