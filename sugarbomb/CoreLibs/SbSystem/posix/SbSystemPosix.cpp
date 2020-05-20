dd8055d5553d39f10d0132b894a02d280ebe1227
BlackPhrase <BlackPhrase@users.noreply.github.com>
2020-05-20 13:35:32 +0000

    [SbSystem] POSIX signals handling support

--- sugarbomb/CoreLibs/SbSystem/posix/SbSystemPosix.cpp
+++ sugarbomb/CoreLibs/SbSystem/posix/SbSystemPosix.cpp
@@ -1,6 +1,8 @@
 /*
 *******************************************************************************
 
+Copyright (C) 1999-2011 id Software LLC, a ZeniMax Media company.
+Copyright (C) 2012 Robert Beckebans
 Copyright (C) 2019-2020 SugarBombEngine Developers
 
 This file is part of SugarBombEngine
@@ -27,5 +29,114 @@ along with SugarBombEngine. If not, see <http://www.gnu.org/licenses/>.
 
 namespace sbe::SbSystem
 {
+
+const int siglist[] =
+{
+	SIGHUP,
+	SIGQUIT,
+	SIGILL,
+	SIGTRAP,
+	SIGIOT,
+	SIGBUS,
+	SIGFPE,
+	SIGSEGV,
+	SIGPIPE,
+	SIGABRT,
+	//SIGTTIN,
+	//SIGTTOU,
+	-1
+};
+
+const char* signames[] =
+{
+	"SIGHUP",
+	"SIGQUIT",
+	"SIGILL",
+	"SIGTRAP",
+	"SIGIOT",
+	"SIGBUS",
+	"SIGFPE",
+	"SIGSEGV",
+	"SIGPIPE",
+	"SIGABRT",
+	//"SIGTTIN",
+	//"SIGTTOUT"
+};
+
+static char fatalError[1024]{};
+
+/*
+===============
+Sys_FPE_handler
+===============
+*/
+void Sys_FPE_handler( int signum, siginfo_t* info, void* context )
+{
+	assert( signum == SIGFPE );
+	Sys_Printf( "FPE\n" );
+};
+
+/*
+================
+Posix_InitSigs
+================
+*/
+void Posix_InitSigs()
+{
+	struct sigaction action;
+	
+	fatalError[0] = '\0';
+	
+	/* Set up the structure */
+	action.sa_sigaction = sig_handler;
+	sigemptyset( &action.sa_mask );
+	action.sa_flags = SA_SIGINFO | SA_NODEFER;
+	
+	int i = 0;
+	
+	while( siglist[ i ] != -1 )
+	{
+		if( siglist[ i ] == SIGFPE )
+		{
+			action.sa_sigaction = Sys_FPE_handler;
+			if( sigaction( siglist[ i ], &action, nullptr ) != 0 )
+				Sys_Printf( "Failed to set SIGFPE handler: %s\n", strerror( errno ) );
+
+			action.sa_sigaction = sig_handler;
+		}
+		else if( sigaction( siglist[ i ], &action, nullptr ) != 0 )
+			Sys_Printf( "Failed to set %s handler: %s\n", signames[ i ], strerror( errno ) );
+
+		i++;
+	};
+	
+	// if the process is backgrounded (running non interactively)
+	// then SIGTTIN or SIGTOU could be emitted, if not caught, turns into a SIGSTP
+	signal( SIGTTIN, SIG_IGN );
+	signal( SIGTTOU, SIG_IGN );
+};
+
+/*
+===============
+Posix_EarlyInit
+===============
+*/
+void Posix_EarlyInit()
+{
+	//memset(&asyncThread, 0, sizeof(asyncThread));
+	
+	exit_spawn[0] = '\0';
+	Posix_InitSigs();
+	
+	// set the base time
+	Sys_Milliseconds();
+	
+	//Posix_InitPThreads();
+};
+
+void SbSystem::PlatformInit()
+{
+	Posix_EarlyInit();
+};
 
 }; // namespace sbe::SbSystem
